cc4e9caef09d19a5129d3fe9cff9060edfb77762
Nick Lee <leexnick@gmail.com>
2019-05-31 11:30:04 -0700

    add jwt auth middleware

---
+++ src/middleware/jwt-auth.js
@@ -0,0 +1,43 @@
+const AuthService = require('../auth/auth-service')
+const UsersService = require('../users/users-service')
+
+function requireAuth(req, res, next) {
+  const authToken = req.get('Authorization') || ''
+
+  let bearerToken
+
+  if(!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({ error: 'Missing bearer token' })
+  } else {
+    bearerToken = authToken.slice(7)
+  }
+
+  try {
+    const payload = AuthService.verifyJwt(bearerToken)
+
+    UsersService.getUserByUsername(
+      req.app.get('db'),
+      payload.sub
+    )
+      .then(user => {
+        if(!user) {
+          return res.status(401).json({ error: 'Unauthorized request' })
+        }
+
+        req.user = user
+        next()
+      })
+      .catch(err => {
+        console.error(err)
+        next(err)
+      })
+  } catch(err) {
+    res.status(401).json({
+      error: 'Unauthorized request'
+    })
+  }
+}
+
+module.exports = {
+  requireAuth
+}
