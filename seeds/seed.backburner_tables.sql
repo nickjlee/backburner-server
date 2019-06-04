699b277d7ce817f330b008c836953f706c74de3f
Nick Lee <leexnick@gmail.com>
2019-06-04 12:08:58 -0700

    reduce seed data

--- seeds/seed.backburner_tables.sql
+++ seeds/seed.backburner_tables.sql
@@ -27,15 +27,6 @@ VALUES
   ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
   ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
   ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
-  ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
-  ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
-  ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
+  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3);
 
 COMMIT;
