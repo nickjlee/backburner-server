d1eb13a1e51f7fdc863853266eb0b20b0816bf27
Nick Lee <leexnick@gmail.com>
2019-06-05 13:46:24 -0700

    remove unique constraint in rewards table

--- migrations/003.do.create_backburner_rewards.sql
+++ migrations/003.do.create_backburner_rewards.sql
@@ -1,5 +1,5 @@
 CREATE TABLE backburner_rewards (
   id SERIAL PRIMARY KEY,
-  reward TEXT NOT NULL UNIQUE,
+  reward TEXT NOT NULL,
   user_id INTEGER REFERENCES backburner_users(id) ON DELETE SET NULL
 );
