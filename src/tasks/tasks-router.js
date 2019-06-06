521beeccf73fee08c8f21aa8dd8b851494660dce
Nick Lee <leexnick@gmail.com>
2019-06-06 08:56:09 -0700

    reorganize tasks router

--- src/tasks/tasks-router.js
+++ src/tasks/tasks-router.js
@@ -6,35 +6,43 @@ const { requireAuth } = require('../middleware/jwt-auth')
 const tasksRouter = express.Router()
 const jsonBodyParser = express.json()
 
-tasksRouter.route('/').get(requireAuth, (req, res, next) => {
-  TasksService.getUserTasks(req.app.get('db'), req.user.id)
-    .then(tasks => {
-      res.json(TasksService.serializeTasks(tasks))
-    })
-    .catch(next)
-})
-
-tasksRouter.route('/').post(requireAuth, jsonBodyParser, (req, res, next) => {
-  const { user_id = req.user.id, text, due_date, reward, xp } = req.body
-  const newTask = { user_id, text, due_date, reward, xp }
-
-  for (const [key, value] of Object.entries(newTask)) {
-    if (value == null) {
-      return res.status(400).json({
-        error: `Missing '${key}' in request body`
+tasksRouter
+  .route('/')
+  .all(requireAuth)
+  .get((req, res, next) => {
+    TasksService.getUserTasks(req.app.get('db'), req.user.id)
+      .then(tasks => {
+        res.json(TasksService.serializeTasks(tasks))
       })
-    }
-  }
+      .catch(next)
+  })
+  .post(jsonBodyParser, (req, res, next) => {
+    const { 
+      user_id = req.user.id,
+      text,
+      due_date,
+      reward,
+      xp
+    } = req.body
+    const newTask = { user_id, text, due_date, reward, xp }
 
-  TasksService.insertNewTask(req.app.get('db'), newTask)
-    .then(task => {
-      res
-        .status(201)
-        .location(path.posix.join(req.originalUrl, `/${user_id}`))
-        .json(TasksService.serializeTask(task))
-    })
-    .catch(next)
-})
+    for (const [key, value] of Object.entries(newTask)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        })
+      }
+    }
+
+    TasksService.insertNewTask(req.app.get('db'), newTask)
+      .then(task => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${user_id}`))
+          .json(TasksService.serializeTask(task))
+      })
+      .catch(next)
+  })
 
 tasksRouter.route('/:task_id')
   .all(checkTaskExists)
