3a178d60156386f2f90ca6989062ad727d467bdd
Nick Lee <leexnick@gmail.com>
2019-06-06 10:14:29 -0700

    allocate seed tasks to demo user

--- seeds/seed.backburner_tables.sql
+++ seeds/seed.backburner_tables.sql
@@ -14,19 +14,8 @@ VALUES
 INSERT INTO backburner_tasks (text, due_date, reward, xp, user_id)
 VALUES
   ('Give the dog a bath', '2019-08-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
-  ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
-  ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
-  ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3),
-  ('Give the dog a bath', '2019-07-01', 'my favorite desert', 50, 1),
-  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 2),
-  ('Finish MVP of First Capstone Project', '2019-06-07', 'a whole day off!', 100, 3);
+  ('Plan the family vacation', '2019-09-30', 'night out with the friends', 75, 1),
+  ('Finish MVP of First Capstone Project', '2019-07-07', 'a whole day off!', 100, 1),
+  ('Administer flea medication for the cat', '2019-10-01', '1h of video game', 50, 1);  
 
 COMMIT;
