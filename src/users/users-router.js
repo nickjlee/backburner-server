36d0e083b18584f90f987c62fe3f8fa9b29cb98c
Nick Lee <leexnick@gmail.com>
2019-06-05 21:11:18 -0700

    add post endpoint to users router and service

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const path = require('path')
 const UsersService = require('./users-service')
 const { requireAuth } = require('../middleware/jwt-auth')
 
 const usersRouter = express.Router()
+const jsonBodyParser = express.json()
 
 usersRouter
   .route('/')
@@ -10,6 +12,56 @@ usersRouter
     UsersService.getUserByUsername(req.app.get('db'), req.user.username)
       .then(user => {
         res.json(UsersService.serializeUser(user))
+      })
+      .catch(next)
+  })
+
+usersRouter
+  .route('/')
+  .post(jsonBodyParser, (req, res, next) => {
+    const { password, username, first_name } = req.body
+
+    for(const field of ['username', 'password', 'first_name']) {
+      if(!req.body[field]) {
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`
+        })
+      }
+    }
+
+    const passwordError = UsersService.validatePassword(password)
+    if(passwordError) {
+      return res.status(400).json({ error: passwordError })
+    }
+
+    UsersService.hasUserWithUserName(
+      req.app.get('db'),
+      username
+    )
+      .then(hasUserWithUserName => {
+        if(hasUserWithUserName) {
+          return res.status(400).json({ error: 'Username is already taken' })
+        }
+
+        return UsersService.hashPassword(password)
+          .then(hashedPassword => {
+            const newUser = {
+              username,
+              password: hashedPassword,
+              first_name
+            }
+
+            return UsersService.insertNewUser(
+              req.app.get('db'),
+              newUser
+            )
+              .then(user => {
+                res
+                  .status(201)
+                  .location(path.posix.join(req.originalUrl))
+                  .json(UsersService.serializeUser(user))
+              })
+          })
       })
       .catch(next)
   })
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,5 +1,8 @@
 const xss = require('xss')
+const bcrypt = require('bcryptjs')
 const Treeize = require('treeize')
+
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 
 const UsersService = {
   getUserByUsername(db, username) {
@@ -9,10 +12,50 @@ const UsersService = {
       .first()
   },
 
+  hasUserWithUserName(db, username) {
+    return db
+      .from('backburner_users')
+      .where({ username })
+      .first()
+      .then(user => !!user)
+  },
+
+  insertNewUser(db, newUser) {
+    return db
+      .insert(newUser)
+      .into('backburner_users')
+      .returning('*')
+      .then(([user]) => user)
+  },
+
+  validatePassword(password) {
+    if (password.length < 8) {
+      return 'Password must be longer than 8 characters'
+    }
+
+    if (password.length > 72) {
+      return 'Password should be less than 72 characters'
+    }
+
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty space'
+    }
+
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return 'Password must contain 1 Uppercase, 1 Lowercase, 1 Number, and 1 Special Character'
+    }
+
+    return null
+  },
+
+  hashPassword(password) {
+    return bcrypt.hash(password, 12)
+  },
+
   serializeUser(user) {
     const userTree = new Treeize()
 
-    const userData = userTree.grow([ user ]).getData()[0]
+    const userData = userTree.grow([user]).getData()[0]
 
     return {
       id: userData.id,
