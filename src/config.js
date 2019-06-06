0165cf996a420b8fa07bfada78cc629ec7d2c04a
Nick Lee <leexnick@gmail.com>
2019-06-06 10:43:26 -0700

    update JWT_SECRET config

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postrgresql://backburner:password@localhost/backburner',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
+  JWT_SECRET: process.env.JWT_SECRET || 'nix-super-secure-secret'
 }
