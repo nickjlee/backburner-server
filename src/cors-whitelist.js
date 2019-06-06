b0bf0513210c303533b33eaf3b36a567d3b3037e
Nick Lee <leexnick@gmail.com>
2019-06-06 11:30:37 -0700

    add client url to cors-whitelist

--- src/cors-whitelist.js
+++ src/cors-whitelist.js
@@ -1,12 +1,17 @@
-'use strict';
+'use strict'
 
-const whitelist = ['http://localhost:3000', 'http://localhost:5000'];
-const originGenerator = function (origin, callback) {
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:5000',
+  'https://backburner.nickjlee.now.sh'
+]
+
+const originGenerator = function(origin, callback) {
   if (whitelist.indexOf(origin) !== -1 || !origin) {
-    callback(null, true);
+    callback(null, true)
   } else {
-    callback(new Error('Not allowed by CORS'));
+    callback(new Error('Not allowed by CORS'))
   }
-};
+}
 
-module.exports = originGenerator;
+module.exports = originGenerator
